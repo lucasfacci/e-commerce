c9cef8d8d7207d918d955165966a8a31005098ff
Lucas <lucasfaccir@outlook.com.br>
2021-10-25 01:33:39 -0300

    Returning more user information

--- backend/core/views.py
+++ backend/core/views.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.compat import coreapi, coreschema
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from rest_framework.schemas import coreapi as coreapi_schema
 from rest_framework.schemas import ManualSchema
 
@@ -94,6 +95,18 @@ class CustomObtainAuthToken(ObtainAuthToken):
             ],
             encoding="application/json",
         )
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'first_name': user.first_name
+        })
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
